a469cbd4c07a7ea69d318eeb1096081510a32052
Anupam Bharti <anupambhartiab@gmail.com>
2023-12-05 23:24:27 +0530

    Added a welcome sceeen to the game

--- Putt-Putt Odyssey/GameViewController.swift
+++ Putt-Putt Odyssey/GameViewController.swift
@@ -14,31 +14,40 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-//                sceneNode.entities = scene.entities
-//                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = false
-                    view.showsNodeCount = false
-                }
+        print("Screen is loaded")
+        if let view = self.view as! SKView? {
+            if let scene = WelcomeScene(fileNamed: "WelcomeScene") {
+                scene.scaleMode = .aspectFill
+                view.presentScene(scene)
             }
         }
+
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+//        if let scene = GKScene(fileNamed: "GameScene") {
+//            
+//            // Get the SKScene from the loaded GKScene
+//            if let sceneNode = scene.rootNode as! GameScene? {
+//                
+//                // Copy gameplay related content over to the scene
+////                sceneNode.entities = scene.entities
+////                sceneNode.graphs = scene.graphs
+//                
+//                // Set the scale mode to scale to fit the window
+//                sceneNode.scaleMode = .aspectFill
+//                
+//                // Present the scene
+//                if let view = self.view as! SKView? {
+//                    view.presentScene(sceneNode)
+//                    
+//                    view.ignoresSiblingOrder = true
+//                    
+//                    view.showsFPS = false
+//                    view.showsNodeCount = false
+//                }
+//            }
+//        }
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
---
+++ Putt-Putt Odyssey/WelcomeScene.swift
@@ -0,0 +1,52 @@
+//
+//  WelcomeScene.swift
+//  Putt-Putt Odyssey
+//
+//  Created by Anu on 05/12/23.
+//
+
+import Foundation
+
+import SpriteKit
+
+class WelcomeScene: SKScene {
+
+    var startGameButton: SKLabelNode!
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .systemIndigo
+
+        // Setup Welcome Message
+        let welcomeLabel = SKLabelNode(fontNamed: "Chalkduster")
+        welcomeLabel.text = "Fairway Saga"
+        welcomeLabel.fontSize = 40
+        welcomeLabel.fontColor = SKColor.white
+        welcomeLabel.position = CGPoint(x: frame.midX, y: frame.midY + 100)
+        addChild(welcomeLabel)
+
+        // Setup Start Game Button
+        startGameButton = SKLabelNode(fontNamed: "Chalkduster")
+        startGameButton.text = "BEGIN"
+        startGameButton.fontSize = 30
+        startGameButton.fontColor = SKColor.green
+        startGameButton.position = CGPoint(x: frame.midX, y: frame.midY - 100)
+        startGameButton.name = "startGameButton"
+        addChild(startGameButton)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let nodes = self.nodes(at: location)
+
+        for node in nodes {
+            if node.name == "startGameButton" {
+                if let gameScene = GameScene(fileNamed: "GameScene") {
+                    gameScene.scaleMode = .aspectFill
+                    view?.presentScene(gameScene, transition: SKTransition.fade(withDuration: 1.0))
+                }
+            }
+        }
+    }
+}
+
